c74639444ed01f6d213001deb469e40b5d70341b
NexusGK Developer <nexusgk3@gmail.com>
2025-01-10 11:32:33 +0300

    `Update firebase.js to use Firestore and export relevant functions`

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"
+import { collection, addDoc, getDocs } from "@firebase/firestore"; // Perbarui ini
+
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCoI5a6pD8NHeYmtX6DxD4XVgG1zj2FDF4",
   authDomain: "portfolio-b9d11.firebaseapp.com",
@@ -18,4 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db, collection, addDoc };
